4359d568a1cacdaeac29b2ce753f5bf8745350b1
Jagdish Salgotra <jagdishsalgotra@Jagdishs-MacBook-Pro.local>
2023-12-24 23:22:34 +0530

    Cors config

--- bookmark-api/src/main/java/js/bookmark/config/WebMvcConfig.java
+++ bookmark-api/src/main/java/js/bookmark/config/WebMvcConfig.java
@@ -1,17 +1,21 @@
 package js.bookmark.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConfigurationProperties(prefix = "web")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/api/**")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PATCH", "PUT")
                 .allowedHeaders("*")
-                .allowedOriginPatterns("*");
+                .allowedOriginPatterns("*")
+                .maxAge(3600);
     }
 }
